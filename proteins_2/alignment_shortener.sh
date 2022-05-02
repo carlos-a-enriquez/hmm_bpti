9c751e0c74dfa5675ff4ee14591ae9548de218d5
Carlos <carlos.enriquez9896@gmail.com>
2022-05-02 19:27:30 +0200

    Initializing the new alignment shortening attempt.

--- proteins_2/alignment_shortener.sh
+++ proteins_2/alignment_shortener.sh
@@ -6,20 +6,25 @@ grep -v -m 1 ">" struct_alignment.seq |awk '{print length}'
 '63'
 
 #writing a awk command to shorten the alignment
-grep -v ">" struct_alignment.seq |awk '{print substr($0,3,length($0)-3)}'|awk '{print length}'
+grep -v ">" struct_alignment.seq |awk '{print substr($0,4,length($0)-4)}'|awk '{print length}'
+'59'
 
 #writing a new command that does not eliminate the headers
-awk '{if (substr($0,0,1)==">") {print $0} else {print substr($0,3,length($0)-3)}}' struct_alignment.seq |less
+awk '{if (substr($0,0,1)==">") {print $0} else {print substr($0,4,length($0)-4)}}' struct_alignment.seq |less
 
 #redirecting into a new alignment file
-awk '{if (substr($0,0,1)==">") {print $0} else {print substr($0,3,length($0)-3)}}' struct_alignment.seq >struct_alignment_corrected.seq
+awk '{if (substr($0,0,1)==">") {print $0} else {print substr($0,4,length($0)-4)}}' struct_alignment.seq >struct_alignment_corrected.seq
 
 #Checking that 3 characters were removed from each sequence
 grep -v ">" struct_alignment_corrected.seq |awk '{print length}'|less
-'60'
+'59'
 
 mv struct_alignment.seq struct_alignment.old
 
 : '
 All good. the alignment was shortened. Now we proceed with the rest of the
-pipeline without any changes (unless explicitly stated down below).'
+pipeline without any changes (unless explicitly stated down below).
+
+The shortening will now eliminate the first 3 characters instead of the first 2.
+2 sequences without gaps is not enough for the model
+'
